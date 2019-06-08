99f83fc735e3f70b4a9ad584938ef7ea89f37638
maydany <ishlove89@naver.com>
2019-06-08 19:56:56 +0900

    [feat] add create_block function

--- blockchain.py
+++ blockchain.py
@@ -17,3 +17,11 @@ class BlockChain:
         self.chain = []
         self.create_block(proof = 1, prev_hash = '0')
 
+    def create_block(self, proof, prev_hash):
+        block = {'index': len(self.chain) + 1, 
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'prev_hash': prev_hash}
+        self.chain.append(block)
+        return block
+        
