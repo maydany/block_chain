a2f008f458c55c1d47063ad2ebe0d0acc0beb903
maydany <ishlove89@naver.com>
2019-06-08 19:46:53 +0900

    [feat] add create_genesis_block function(__init__)

--- blockchain.py
+++ blockchain.py
@@ -9,5 +9,11 @@ import hashlib
 import json
 from flask import Flask, jsonify
 
-#BLOCK CHAIN INIT
+#BUILD BLOCK CHAIN
+
+class BlockChain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = '0')
 
