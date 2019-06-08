4a5be99725e861e9aec17cc2ba8f277f70ad48ae
maydany <ishlove89@naver.com>
2019-06-08 20:32:25 +0900

    [feat] add proof_of_work function

--- blockchain.py
+++ blockchain.py
@@ -15,15 +15,26 @@ class BlockChain:
     
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, prev_hash = '0')
+        self.create_block(nounce = 1, prev_hash = '0')
 
-    def create_block(self, proof, prev_hash):
+    def create_block(self, nounce, prev_hash):
         block = {'index': len(self.chain) + 1, 
                  'timestamp': str(datetime.datetime.now()),
-                 'proof': proof,
+                 'nounce': nounce,
                  'prev_hash': prev_hash}
         self.chain.append(block)
         return block
     
     def get_prev_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self, prev_nounce):
+        new_nounce = 1
+        check_nounce = False
+        while check_nounce is False:
+            hash_operation = hashlib.sha256(str(new_nounce**2 - prev_nounce**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_nounce = True
+            else:
+                new_nounce += 1
+        return new_nounce
